773855444db0248cb8ea055cb1bc52cd88649978
Paulo Ricardo <prmaltaleal@gmail.com>
2023-12-27 07:06:33 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,16 @@
+function skillMember() {
+      var member = {
+    name: 'John',
+    age: 30,
+    skills: ['JavaScript', 'CSS', 'HTML'],
+    address: {
+      street: '123 Main St',
+      city: 'Boston',
+      state: 'MA'
+    },
+    getBirthYear: function() {
+      return 2017 - this.age;
+    }
+  }
+  return member;
+}
